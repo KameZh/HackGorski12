1389f37f1c2e67539140568f1a8369bf3d246014
KameZh <cyberbush720@gmail.com>
2026-03-27 10:01:03 +0200

    fix for hosting

--- pytechka-frontend/vite.config.js
+++ pytechka-frontend/vite.config.js
@@ -27,6 +27,7 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 5173,
     strictPort: true,
+    allowedHosts: ['4276-194-141-252-114.ngrok-free.app', 'all'],
     hmr: {
       port: 5173,
     },

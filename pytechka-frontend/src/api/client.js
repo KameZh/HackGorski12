ff5761ee4771f2edba290e603e20974744b4d804
Kris Al Tawil <kris.altawil@gmail.com>
2026-03-27 13:39:29 +0200

    small changes

--- pytechka-frontend/src/api/client.js
+++ pytechka-frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:5174/api',
+  baseURL: '/api',
 })
 
 // Clerk stores its session token accessible via this global.
